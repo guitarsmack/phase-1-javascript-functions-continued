435a6b40de8f5abda07b12cd2bf8f2edf8891cad
Michael Alan Stowe <mastowe94@gmail.com>
2023-06-06 18:06:19 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(param = "roller-skate") {
+    return `This Saturday, I want to ${param}!`
+}
+
+function mondayWork(argue = "go to the office") {
+    return `This Monday, I will ${argue}.`;
+}
+
+function wrapAdjective(example = "*") {
+    return function (word = "special"){
+        return `You are ${example}${word}${example}!`
+    }
+}
